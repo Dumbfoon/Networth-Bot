e47ca65e3b39a361cbc2772b1799a33fbef92745
Dumbfoon <conradgreulich@gmail.com>
2025-04-09 19:22:18 +0100

    updated bot
    idfk

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -1,48 +1,40 @@
 #include "MyBot.h"
 #include <dpp/dpp.h>
 
-/* Be sure to place your token in the line below.
- * Follow steps here to get a token:
- * https://dpp.dev/creating-a-bot-application.html
- * When you invite the bot, be sure to invite it with the 
- * scopes 'bot' and 'applications.commands', e.g.
- * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
- */
-const std::string    BOT_TOKEN    = "add your token here";
-
-
-//viktor rex the goat
+const std::string BOT_TOKEN = "<enter token here>";
 
 int main()
 {
-	/* Create bot cluster */
-	dpp::cluster bot(BOT_TOKEN);
+    dpp::cluster bot(BOT_TOKEN);
 
-	/* Output simple log messages to stdout */
-	bot.on_log(dpp::utility::cout_logger());
+    bot.on_log(dpp::utility::cout_logger());
 
-	/* Register slash command here in on_ready */
-	bot.on_ready([&bot](const dpp::ready_t& event) {
-		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
-		if (dpp::run_once<struct register_bot_commands>()) {
-			std::vector<dpp::slashcommand> commands {
-				{ "ping", "Ping pong!", bot.me.id }
-			};
+    
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event)
+    {
+        
+        if (event.command.get_command_name() == "networth") 
+        {
+            
+            std::string username = std::get<std::string>(event.get_parameter("username"));
 
-			bot.global_bulk_command_create(commands);
-		}
-	});
+            event.reply(username + " is the goat.");
+        }
+    });
 
-	/* Handle slash command with the most recent addition to D++ features, coroutines! */
-	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
-		if (event.command.get_command_name() == "ping") {
-			co_await event.co_reply("Pong!");
-		}
-		co_return;
-	});
+    bot.on_ready([&bot](const dpp::ready_t& event) 
+    {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            dpp::slashcommand newcommand("networth", "Get networth of someone", bot.me.id);
+            newcommand.add_option(
+                dpp::command_option(dpp::co_string, "username", "IGN of person", true)
+            );
 
-	/* Start the bot */
-	bot.start(dpp::st_wait);
+            bot.global_command_create(newcommand);
+        }
+    });
 
-	return 0;
+    bot.start(dpp::st_wait);
+
+    return 0;
 }
